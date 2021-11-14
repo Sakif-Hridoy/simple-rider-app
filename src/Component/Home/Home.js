91f59d7861130845f5ecf73ff872fb5f3cfe67a7
Sakif Hridoy <sakifhridoy@gmail.com>
2021-11-14 19:44:13 +0600

    second-commit

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
+import FakeData from '../FakeData/vehicle.json';
+
 const Home = () => {
+    const [vehicles,setVehicle] = useState([]);
+
+    useEffect(()=>{
+        const loadData = FakeData;
+    })
     return (
         <div className="home">
             <h1>This is Home</h1>
